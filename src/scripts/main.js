d74265f2fa080af5f7ed7167cd71d34a9321d62f
Rafael <Rafael@git.com>
2024-05-13 18:08:12 -0300

    contador

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,25 @@
 
-console.log("Olá");
+AOS.init();
+
+const dataDoEvento = new Date("Dec 12, 2024 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function() {
+    const agora = new Date ();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / (1000 * 60 * 60 * 24));
+    const horasAteOEvento = Math.floor((diasAteOEvento % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutosAteOEvento = Math.floor(distanciaAteOEvento % (1000 * 60 * 60)) / (1000 * 60);
+    const segundosAteOEvento = Math.floor ((distanciaAteOEvento % (1000 * 60)) / 1000);
+
+    console.log (diasAteOEvento);
+    console.log (horasAteOEvento);
+    console.log (minutosAteOEvento);
+    console.log (segundosAteOEvento);
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+
+}, 1000);
